784e5f62f7ec483d1839b088279e8afeb41e91d6
piatsko <andreypiatsko@gmail.com>
2022-04-17 00:51:06 +0300

    lab_3: completed c++98 implementation, added .bat file

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -25,7 +25,28 @@ vector<HANDLE> start_threads(int count){
     return threads_handles;
 }
 
+bool getBoolArrValue(vector<bool>& v){
+    bool answ = 1;
+    for (int i = 0; i < v.size(); i++)
+        if (isActive[i])
+            answ &= v[i];
+    return answ;
+}
+
+void setIsLocked(vector<bool>& v){
+    for (int i = 0; i < v.size(); i++)
+        if (isActive[i])
+            v[i] = 0;
+}
+
+void print(vector<int>& v){
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+}
+
 int main() {
+
     cout << "Enter array size: ";
     int arr_size; cin >> arr_size;
     array = vector<int>(arr_size, 0);
@@ -33,14 +54,26 @@ int main() {
     cout << "Enter markers count: ";
     int marker_count; cin >> marker_count;
 
+    isLocked = vector<bool>(marker_count, 0);
+    isActive = vector<bool>(marker_count, 1);
+    vector<bool> isClosed(marker_count, 0);
+
     vector<HANDLE> threads_handles = start_threads(marker_count);
 
-    for (int i = 0; i < array.size(); i++)
-        WaitForSingleObject(threads_handles[i], INFINITE);
+    while (!getBoolArrValue(isClosed)){
+        while (!getBoolArrValue(isLocked));
 
-    int active_markers = marker_count;
+        print(array);
+
+        cout << "Enter # of marker to be closed: " << endl;
+        int num; cin >> num;
+        isActive[num - 1] = 0;
+        isClosed[num - 1] = 1;
+        WaitForSingleObject(threads_handles[num - 1], INFINITE);
+
+        setIsLocked(isLocked);
+    }
 
     cout << "RESULT ARRAY" << endl;
-    for (int i = 0; i < array.size(); i++)
-        cout << array[i] << " ";
+    print(array);
 }
--- lab_3/marker.h
+++ lab_3/marker.h
@@ -13,38 +13,59 @@ using namespace std;
 
 static vector<int> array;
 
+static vector<bool> isLocked;
+static vector<bool> isActive;
+
+int lock = 0;
+
+void xchg(register int& r, int& x){
+    int temp;
+
+    temp = r;
+    r = x;
+    x = temp;
+}
+
 struct marker_params{
-    marker_params(int index_)
+    marker_params(int number_)
     {
-        index = index_;
-        marked = 0;
+        number = number_;
     }
-    int index;
-    int marked;
+    int number;
 };
+
 
 DWORD WINAPI marker(void* params_){
     marker_params params = *reinterpret_cast<marker_params*>(params_);
-    int index = params.index;
-    int& marked =  params.marked;
-    srand(index);
+    int number = params.number;
+    int marked = 0;
+    srand(number);
 
-    while (true) {
-        int random = rand();
-        random %= array.size();
-
-        if (array[random] == 0) {
-            Sleep(5);
-            array[random] = index;
-            Sleep(5);
-            marked++;
-        } else {
-            cout << "Marker index: " << index
-                 << "\tMarked elements: " << marked
-                 << "\tCan't mark: " << random
-                 << endl;
-            break;
+    register int key = 1;
+    while (isActive[number - 1]) {
+        while(key == 1)
+            xchg(key, lock);
+        while (!isLocked[number - 1]) {
+            int random = rand();
+            random %= array.size();
+            if (array[random] == 0) {
+                Sleep(5);
+                array[random] = number;
+                Sleep(5);
+                marked++;
+            } else {
+                printf("Marker number: %d\tMarked elements: %d\t Can't mark: %d\n", number, marked, random + 1);
+                isLocked[number - 1] = 1;
+            }
         }
+        lock = 0;
     }
+
+    for (int i = 0; i < array.size(); i++){
+        if (array[i] == number)
+            array[i] = 0;
+    }
+
     return 0;
 }
+#pragma clang diagnostic pop
